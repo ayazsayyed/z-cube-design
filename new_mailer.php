c91fa4f3977da1037151cf21b652f18226cae6cf
T-Fusion-Technologies <tfusion.tech@gmail.com>
2024-03-01 20:08:33 +0530

    [Contact-Us] -> Form and emailer

---
+++ new_mailer.php
@@ -0,0 +1,56 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+include 'assets/emailers/contact-us/email-body.php';
+$nameErr = $contactErr = $emailErr = $messageErr = $captchaErr = "";
+$name = $contact = $email = $message = $captcha = "";
+$emailSent = false;
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $contact = test_input($_POST["contact"]);
+    $message = test_input($_POST["message"]);
+
+    $mail = new PHPMailer(true);
+
+    var_dump($contact);
+    var_dump($name);
+    $emailBody = generateEmailBody($name, $contact, $email, $message);
+    // echo($emailBody);
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();   
+        $mail->Host = 'ssl://smtp.gmail.com:465';                                         //Send using SMTP
+        // $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'ayaz.sayyed94@gmail.com';                     //SMTP username
+        $mail->Password   = 'cphyseilzxvbqlrp';                               //SMTP password
+        $mail->SMTPSecure = false;
+        $mail->Port       = 587;
+        $mail->setFrom('ayaz.sayyed94@gmail.com', 'Zcube Design');
+        $mail->addAddress('ayaz.sayyed94@gmail.com');
+        $mail->addAddress('ayaz.sayyed94@gmail.com', 'Ayaz Sayyed' );
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Congratulations! New Enquiry From Zcubedesign.com';
+        $mail->Body = $emailBody;
+        $mail->send();
+        $name = $contact = $email = $message = '';
+        $emailSent = true;
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+?>
--- quick_inquiry.php
+++ quick_inquiry.php
@@ -1,42 +1,40 @@
+<form name="contactform"  method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform" onSubmit="return validateForm()">
+	<div class="col-md-12">
+		<div class="form-group">
+			<label for="name">Name</label>
+			<input type="text" class="form-control" id="name" name="name" placeholder="Name..." value="<?php echo $name;?>">
+			<span id="nameErr"  class="error-message"><?php echo $nameErr;?></span>
+		</div>
+	</div>
+	<div class="col-md-12">
+		<div class="form-group">
+			<label for="phone">Contact</label>
+			<input type="text" class="form-control" id="contact" name="contact" placeholder="Contact..." value="<?php echo $contact;?>">
+			<span id="contactErr" class="error-message"><?php echo $contactErr;?></span>									
+		</div>					
+	</div>
+	<div class="col-md-12">
+		<div class="form-group">
+			<label for="email">Email-ID</label>
+			<input type="email" class="form-control" id="email" name="email" placeholder="Email..." value="<?php echo $email;?>">
+			<span id="emailErr" class="error-message"><?php echo $emailErr;?></span>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-lg-12">
+			<label>Message </label>
+			<textarea type="textbox" class="form-control" id="message" name="message" placeholder="Inquiry..." value="<?php echo $message;?>"/></textarea>
+			<span id="messageErr" class="error-message"><?php echo $messageErr;?></span>
+		</div>
+	</div>
+	<div class="col-lg-12 form-group submit">
 
-	<form name="contactform"  method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform" onSubmit="return validatePopUpForm(this)">
-		<div class="col-md-12">
-			<div class="form-group cust-error">
-				<label for="name">Name</label>
-				<input type="text" class="form-control" id="name" name="name" placeholder="Name..." value="<?php echo $name;?>">
-				<span id="nameErr"  class="error-message"><?php echo $nameErr;?></span>
-			</div>
-		</div>
-		<div class="col-md-12">
-			<div class="form-group">
-				<label for="phone">Contact</label>
-				<input type="text" class="form-control" id="contact" name="contact" placeholder="Contact..." value="<?php echo $contact;?>">
-				<span id="contactErr" class="error-message"><?php echo $contactErr;?></span>									
-			</div>					
-		</div>
-		<div class="col-md-12">
-			<div class="form-group">
-				<label for="email">Email-ID</label>
-				<input type="email" class="form-control" id="email" name="email" placeholder="Email..." value="<?php echo $email;?>">
-				<span id="emailErr" class="error-message"><?php echo $emailErr;?></span>
-			</div>
-		</div>
-		<div class="form-group">
-			<div class="col-lg-12">
-				<label>Message </label>
-				<textarea type="textbox" class="form-control" id="message" name="message" placeholder="Inquiry..." value="<?php echo $message;?>"/></textarea>
-				<span id="messageErr" class="error-message"><?php echo $messageErr;?></span>
-			</div>
-		</div>
-		<div class="form-group">
-			<div class="col-lg-12">
-				<div class="g-recaptcha" data-sitekey="6LdyBccUAAAAAJ5wMRUF-6Lzh9Jvhu8BI82l-eQV"></div>
-				<span id="captchaErr" class="error-message"><?php echo $captchaErr;?></span>
-			</div>
-		</div>
-		<div class="col-lg-12 form-group submit">
-			<input type="button" name="submit" class="btn block more_about_us dark" value="Submit" data-toggle="modal" data-target="#thankyou-sub">
-		</div>	
-		
-		<div class="clear"> </div>
-	</form>
+	<button class="btn" type="submit" data-sitekey="6LeKpoApAAAAACGa6z_aNmQ3QnIjsrsf0PE1HxSF" 
+			data-callback='onSubmit' 
+			data-action='submit'>
+			Submit
+  <span class="spinner-border-sm" role="status" aria-hidden="true"></span>
+</button>
+	</div>					
+	<div class="clear"> </div>
+</form>
